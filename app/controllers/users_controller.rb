74ce2f12bcf67cde8c8a961d502767aac660977b
OGM <nogmrgmr@gmail.com>
2019-07-15 13:57:25 +0900

    715 フラッシュメッセージでバグ発生
    right　の下に潜る
    ＆
    スクロールしない

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,31 @@
+class UsersController < ApplicationController
+  
+  def edit
+  end
+
+  def update
+    # binding.pry
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
+  end
+
+  def create
+    binding.pry
+    @member = Member.new(member_params)
+    if @member.save
+      flash[:notice] = "アカウントが作成されました"
+      redirect_to members_path
+    else
+      render :edit
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
+end
+
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,18 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'messages#index'
+  resources :messages, only: [:index]
+  resources :users, onlu:[:edit, :update, :create]
+  
 end
+
+
+
+# Rails.application.routes.draw do
+#   devise_for :users
+#   root "tweets#index"
+#   resources :tweets, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
+#   resources :comments, only: [:create]
+#   end
+#   resources :users, only: [:show]
+# end
