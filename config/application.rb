a331d2df16f68f0ceabdd865973f62c8384d9fde
OGM <nogmrgmr@gmail.com>
2019-07-20 14:49:28 +0900

    model 作成　ルーティング設定

--- config/application.rb
+++ config/application.rb
@@ -8,6 +8,7 @@ Bundler.require(*Rails.groups)
 
 module ChatSpace
   class Application < Rails::Application
+    config.i18n.default_locale = :ja
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
@@ -17,5 +18,6 @@ module ChatSpace
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-  end
+    end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,19 +1,13 @@
 Rails.application.routes.draw do
+  # devise_for :groups
+  # devise_for :group_user
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'messages#index'
-  resources :messages, only: [:index]
-  resources :users, onlu:[:edit, :update, :create]
-  
+  resources :users, only:[:index, :edit, :update]
+  resources :groups,only:[:new, :create, :edit, :update] do
+   resources :messages, only: [:index, :create]
+  end
 end
 
 
-
-# Rails.application.routes.draw do
-#   devise_for :users
-#   root "tweets#index"
-#   resources :tweets, only: [:index, :show, :new, :create, :destroy, :edit, :update] do
-#   resources :comments, only: [:create]
-#   end
-#   resources :users, only: [:show]
-# end
