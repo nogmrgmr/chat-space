516bd4d94e81f29470a27126c5cbd025ff9cfc75
OGM <nogmrgmr@gmail.com>
2019-08-04 13:52:29 +0900

    追加ボタンで移動　実装完了
    ようやく
    append と attr 　の使い間違い
    単純だけど一生気づかなかっった
    正しいと思い込んでいるところが致命的

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -4,15 +4,15 @@ $(document).on('turbolinks:load', function(){
   function userFind(user){
     var html = `<div class="chat-group-user clearfix">
                 <p class="chat-group-user__name">${user.name}</p>
-                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="ユーザーのid" data-user-name="ユーザー名">追加</div>
+                <div class="user-search-add chat-group-user__btn chat-group-user__btn--add" data-user-id="${user.id}" data-user-name="${user.name}">追加</div>
                </div>`
     return html;
   }
 
   function addUser(user){
     var html = `<div class='chat-group-user'>
-                  <input name='group[user_ids][]' type='hidden' value='ユーザーのid'>
-                  <p class='chat-group-user__name'>${user.name}</p>
+                  <input name='group[user_ids][]' type='hidden' value='${id}'>
+                  <p class='chat-group-user__name'>${name}</p>
                   <div class='user-search-remove chat-group-user__btn chat-group-user__btn--remove js-remove-btn'>削除</div>
                 </div>`
     return html; 
@@ -21,7 +21,6 @@ $(document).on('turbolinks:load', function(){
     $("#user-search-field").on("keyup", function(e){
         e.preventDefault();
       var input = $("#user-search-field").val();
-      console.log(input);
       $.ajax({
         url: '/users',
         type: "GET",
@@ -43,27 +42,38 @@ $(document).on('turbolinks:load', function(){
       .fail(function(){
         alert('NOT JSON');
       })
-      });   
+    });   
 
-      $(document).on("click",".chat-group-user__btn--add",function(e){
-        e.preventDefault();
-        var formData = new FormData();
-      $.ajax({
-        url: '/users',
-        type: 'GET',
-        data: formData,
-        dataType: 'json',
-        processData: false,
-        contentType: false
-      })
-      .done(function(user){
-        alert('JSON COME!')
-        var html = addUser(user);
-        $('#chat-group-users').append(html)
-        $('.').remove();
-      })
-      .fail(function(){
-        alert('NOT JASONNNNN')
-      })
-     })
-    });
+      $(document).on("click",".chat-group-user__btn--add",function(){
+        var id =  $(this).attr('data-user-id')
+        var name = $(this).attr('data-user-name')
+        var html = addUser(id,name);
+        $('#chat-group-users').append(html);
+        $(this).parent().remove();
+      });
+});
+
+
+
+// 非同期通信でやろうとしてたけど結局非同期通信しなくてよかったけど残してるやつ
+// 非同期通信でできないか試すために残しとく用
+// $(document).on("click",".chat-group-user__btn--add",function(e){
+//   $('.chat-group-form__input').submit();
+//   e.preventDefault();
+// $.ajax({
+//   url: '/users',
+//   type: 'GET',
+//   dataType: 'json',
+//   processData: false,
+//   contentType: false
+// })
+// .done(function(user){
+//   alert('JSON COME!')
+//   var html = addUser(user);
+//   $('#chat-group-users').append(html)
+//   // $('親要素ごと').remove();
+// })
+// .fail(function(){
+//   alert('NOT JASONNNNN')
+// })
+// })
