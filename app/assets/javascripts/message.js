7eb7f1ceab5064eb5be0b7ed5d4e8c5da5baca5b
OGM <nogmrgmr@gmail.com>
2019-08-08 14:52:14 +0900

    v

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -77,6 +77,7 @@ $(document).on('turbolinks:load', function(){
       alert('エラー');
       $('.send').prop('disabled', false);
   })
+
   var reloadMessages = function () {
     if (window.location.href.match(/\/groups\/\d+\/messages/)){
       last_message_id = $('.message:last').data("id");
